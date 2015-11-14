21c25e9dca35e8a7833ea3795c6f2cd407c0ced8
Bruno Lottero <brunoelottero@gmail.com>
2015-11-13 23:34:41 -0300

    Parser for reading very large logs in chunks

---
+++ tempura.py
@@ -0,0 +1,19 @@
+__author__ = 'Bruno Lottero'
+
+
+def main():
+    chunksize = 10
+    accesslog_parser(chunksize)
+
+
+def accesslog_parser(chunksize):
+    chunk = []
+    with open('access.log', 'r') as accesslog:
+        for line in accesslog:
+            chunk.append(line.split())
+            if len(chunk) == chunksize:
+                print 'Printing a chunk \n'
+                print chunk
+                #process(chunk) // Do something with this chunk
+                chunk = []
+
